3f11cf44272ca97147a86d4b69f3d0ee119d3662
jdedji <jeandedji2016@gmail.com>
2020-07-14 19:02:40 -0400

    ExpressMongoDB

---
+++ nucampsiteServer/routes/partnerRouter.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Partner = require('../models/partner');
+
+const partnerRouter = express.Router();
+
+partnerRouter.use(bodyParser.json());
+
+//BASE ROUTE is when there are no params in the request
+partnerRouter.route('/')
+// .all((req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+// })
+.get((req, res, next) => {
+    Partner.findById(req.params.partners)
+    .then(partner => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(partner);
+ })  
+    .catch(err => next(err));      
+    // res.end('Will send all the partner to you');
+})
+.post((req, res, next) => {
+    Partner.create(req.body)
+    .then(partner => {
+        console.log('Partner Created ', partner);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+    .catch(err => next(err));
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partners');
+})
+.delete((req, res, next) => {
+    Partner.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+});
+
+//Runs when there is a parameter in the request
+partnerRouter.route('/:partners')
+// .all((req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+// })
+.get((req, res, next) => {
+    Partner.findById(req.params.partners)
+    .then(partner => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+    .catch(err => next(err));
+})
+
+.post((req, res, next) => {
+    Partner.create(req.body)
+    .then(partner => {
+        console.log('Partner Created ', partner);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+    .catch(err => next(err));
+})
+
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT '+req.params.partners);
+})
+
+.delete((req, res, next) => {
+    Partner.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+});
+
+
+
+
+
+module.exports = partnerRouter;
+
---
+++ nucampsiteServer/routes/promotionRouter.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Promotion = require('../models/promotion');
+
+const promotionRouter = express.Router();
+
+promotionRouter.use(bodyParser.json());
+
+//BASE ROUTE
+promotionRouter.route('/')
+
+// .all((req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+// })
+.get((req, res, next) => {
+    Promotion.findById(req.params.promotions)
+    .then(promotion => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion.comments);
+ })  
+    .catch(err => next(err));      
+    // res.end('Will send all the Promotion to you');
+})
+.post((req, res, next) => {
+    Promotion.create(req.body)
+    .then(promotion => {
+        console.log('Promotion Created ', promotion);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    })
+    .catch(err => next(err));
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promotions');
+})
+.delete((req, res, next) => {
+    Promotion.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+});
+
+//Runs when there is a parameter in the request
+promotionRouter.route('/:promotions')
+// .all((req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+// })
+.get((req, res, next) => {
+    Promotion.findById(req.params.promotions)
+    .then(promotion => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    })
+    .catch(err => next(err));
+})
+
+.post((req, res, next) => {
+    Promotion.create(req.body)
+    .then(promotion => {
+        console.log('Promotion Created ', promotion);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    })
+    .catch(err => next(err));
+})
+
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT '+req.params.promotions);
+})
+
+.delete((req, res, next) => {
+    Promotion.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
+});
+
+
+// .all((req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+// })
+// .get((req, res) => {
+//     res.end('Will send all the promotion to you');
+// })
+// .post((req, res) => {
+//     res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+// })
+// .put((req, res) => {
+//     res.statusCode = 403;
+//     res.end('PUT operation not supported on /campsites');
+// })
+// .delete((req, res) => {
+//     res.end('Deleting all campsites');
+// });
+
+
+// campsiteRouter.route('/:promotions')
+// .all((req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+// })
+// .get((req, res) => {
+//     // res.end('Will send all the campsites to you');
+//     res.end("Get promotion: "+req.params.promotions)
+// })
+// .post((req, res) => {
+//     res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+// })
+// .put((req, res) => {
+//     res.statusCode = 403;
+//     res.end('PUT '+req.params.promotions);
+// })
+// .delete((req, res) => {
+//     res.end('Deleting promotion: '+req.params.promotions );
+// });
+
+
+
+
+
+module.exports = promotionRouter;
+
